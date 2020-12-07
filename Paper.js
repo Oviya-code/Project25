5d2654d67717f1d03ea5dfcea5d2adfbd57cfff3
Oviya-code <72811234+Oviya-code@users.noreply.github.com>
2020-12-07 18:40:57 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,41 @@
+class Paper {
+    constructor(x,y,r) {
+        var options={
+            'isStatic':false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2
+         }
+        this.body=Bodies.circle(this.x,this.y,this.r,options);
+        World.add(world, this.Body);
+
+    }
+     display(){
+        var paperpos=this.body.position;
+        push();
+        translate(paperpos.x,paperpos.y);
+        strokeWeight(3);
+        fill(255,0,255)
+        ellipse(0,0,this.r, this.r);
+        pop();
+     }
+ };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
